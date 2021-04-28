183affd8b8575637d7eac3554370a87560e2be84
Jan Gajewski <jasqs@github.com>
2021-04-28 21:44:27 +0200

    pip setup

---
+++ dokr
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+
+print("hey there, this is my first pip package")
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name='dokr',
+    version='0.1',
+    scripts=['dokr'],
+    author="Jan Gajewski",
+    author_email="jan.gajewski@ifj.edu.pl",
+    description="bla bla bla",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/javatechy/dokr",
+    packages=setuptools.find_packages(),
+    classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+    ],
+)
